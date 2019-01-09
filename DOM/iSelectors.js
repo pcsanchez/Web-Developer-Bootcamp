b13d3df1989340a6960008160791aca5eaee3bc6
Paco Sanchez <paco.sanchez.ram@gmail.com>
2019-01-08 22:51:09 -0600

    Finished js selectors excercise

---
+++ DOM/iSelectors.js
@@ -0,0 +1,6 @@
+var tag = document.getElementById("highlight");
+var tags = document.getElementsByClassName("bolded");
+var tags = document.getElementsByTagName("li");
+var tag = document.querySelector("#highlight");
+var tags = document.querySelectorAll("h1");
+var tags = document.querySelectorAll(".bolded")
