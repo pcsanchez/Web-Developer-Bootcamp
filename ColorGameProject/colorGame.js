8c54fb737863d23feb8ffeb076296eb1134cc8f2
Paco Sanchez <paco.sanchez.ram@gmail.com>
2019-01-10 21:39:52 -0600

    Finished styling the color game

--- ColorGameProject/colorGame.js
+++ ColorGameProject/colorGame.js
@@ -1,51 +1,106 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+
+var colors = generateRandomColors(numSquares);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function () {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function () {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+
+    }
+});
+
+resetButton.addEventListener("click", function () {
+    //generate all new colors
+    colors = generateRandomColors(numSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change color display to match picked color
+    colorDisplay.textContent = pickedColor;
+    //change color of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+});
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0;i < squares.length;i++){
+for (var i = 0; i < squares.length; i++) {
     //Add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
 
     //Add click listeners to squares
-    squares[i].addEventListener("click",function(){
+    squares[i].addEventListener("click", function () {
         //grab color of clicked square
         var clickedColor = this.style.backgroundColor;
         //compare color to picked color
-        if(clickedColor === pickedColor){
+        if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
-        } else{
+        } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
         }
     });
 }
 
-function changeColors(color){
+function changeColors(color) {
     //loop through all squares
-    for(var i = 0;i < squares.length;i++){
+    for (var i = 0; i < squares.length; i++) {
         //change each color to match given color
         squares[i].style.backgroundColor = color;
     }
 }
 
-function pickColor(){
+function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
 
-function generateRandomColors(num){
+function generateRandomColors(num) {
     //make an array
     var arr = []
     //repeat num times
-    for(var i = 0;i < num;i++){
+    for (var i = 0; i < num; i++) {
         //get random color and push into array
         arr.push(randomColor());
     }
@@ -53,7 +108,7 @@ function generateRandomColors(num){
     return arr;
 }
 
-function randomColor(){
+function randomColor() {
     //pick a "red" from 0 - 255
     var r = Math.floor(Math.random() * 256);
     //pick a "green" from 0 - 255
