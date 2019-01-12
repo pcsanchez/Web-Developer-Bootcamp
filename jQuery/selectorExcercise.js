8c46c29224dde03b56c741e4f4c752b3808a4ddc
Paco Sanchez <paco.sanchez.ram@gmail.com>
2019-01-11 21:07:36 -0600

    Finished intro to jQuery

---
+++ jQuery/selectorExcercise.js
@@ -0,0 +1,12 @@
+//Select all divs and give them a purple background
+$("div").css("backgroundColor", "purple");
+
+//Select the divs with class "highlight" and make then 200px wide
+$("div.highlight").css("width", "200px");
+
+//Select the div with id "third" and give it an organge border
+$("#third").css("border", "2px solid orange");
+
+//Select only the first div and change its font color to pink
+
+$("div:first-of-type").css("color", "pink");
