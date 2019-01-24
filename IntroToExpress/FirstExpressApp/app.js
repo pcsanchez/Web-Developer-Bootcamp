2bdf1f0e3c118f81cb8b37de420e78a4c77b3169
Paco Sanchez <paco.sanchez.ram@gmail.com>
2019-01-24 12:42:40 -0600

    Express and package.json demo

---
+++ IntroToExpress/FirstExpressApp/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+// "/" => "HI THERE!"
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+// "/bye" => "GOODBYE!"
+app.get("/bye",function(req, res){
+    res.send("Goodbye!");
+});
+// "/dog" => "MEOW!"
+app.get("/dog",function(req, res){
+    console.log("Request made for dog");
+    res.send("MEOW!");
+})
+
+//Tell express to listen for requests (start server)
+
+app.listen(3000,function(){
+    console.log("Started server on port 3000")
+});
