342971d1615f94b19671deda32bb407a2cc21d41
Paco Sanchez <paco.sanchez.ram@gmail.com>
2019-01-28 18:41:05 -0600

    Finished Partials lessons

--- FirstEjs/app.js
+++ FirstEjs/app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/",function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fellinlovewith/:thing",function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs",{thingVar: thing});
+    res.render("love",{thingVar: thing});
 })
 
 app.get("/posts", function(req, res){
@@ -17,7 +20,7 @@ app.get("/posts", function(req, res){
         {title: "Can you believe this pomsky!", author: "Colt"}
     ];
 
-    res.render("posts.ejs", {posts: posts})
+    res.render("posts", {posts: posts})
 });
 
 app.listen(3000,function(){
