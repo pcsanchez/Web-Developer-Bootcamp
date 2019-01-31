669351a10d4d1c5a10946a45d91c9f9b4e3d35b1
Paco Sanchez <paco.sanchez.ram@gmail.com>
2019-01-31 12:08:58 -0600

    Finished Movie API application

--- API/movie_search_app/app.js
+++ API/movie_search_app/app.js
@@ -2,10 +2,19 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb",function(error, response, body){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"
+    request(url,function(error, response, body){
         if(!error && response.statusCode == 200){
-            res.send(body);
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
         }
     });
 });
